ef0ae365c873f8cb60b3fdb218e43d1f68f37bb5
Theodore Wong <twong@humanlongevity.com>
2017-01-19 10:29:24 -0800

    Created personal Play tutorial repo

---
+++ app/controllers/Widgets.scala
@@ -0,0 +1,58 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.modules.reactivemongo.{ MongoController, ReactiveMongoApi, ReactiveMongoComponents }
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.bson.{ BSONObjectID, BSONDocument }
+import repos.WidgetRepoImpl
+
+object WidgetFields {
+  val Id = "_id"
+  val Name = "name"
+  val Description = "description"
+  val Author = "author"
+}
+
+class Widgets @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends Controller
+    with MongoController with ReactiveMongoComponents {
+
+  import controllers.WidgetFields._
+
+  def widgetRepo = new WidgetRepoImpl(reactiveMongoApi)
+
+  def index = Action.async { implicit request =>
+    widgetRepo.find().map(widgets => Ok(Json.toJson(widgets)))
+  }
+
+  def create = Action.async(BodyParsers.parse.json) { implicit request =>
+    val name = (request.body \ Name).as[String]
+    val description = (request.body \ Description).as[String]
+    val author = (request.body \ Author).as[String]
+    widgetRepo.save(BSONDocument(
+      Name -> name,
+      Description -> description,
+      Author -> author)).map(result => Created)
+  }
+
+  def read(id: String) = Action.async { implicit request =>
+    widgetRepo.select(BSONDocument(Id -> BSONObjectID(id))).map(widget => Ok(Json.toJson(widget)))
+  }
+
+  def update(id: String) = Action.async(BodyParsers.parse.json) { implicit request =>
+    val name = (request.body \ Name).as[String]
+    val description = (request.body \ Description).as[String]
+    val author = (request.body \ Author).as[String]
+    widgetRepo.update(BSONDocument(
+      Id -> BSONObjectID(id)),
+      BSONDocument("$set" -> BSONDocument(Name -> name, Description -> description, Author -> author))).map(result => Accepted)
+  }
+
+  def delete(id: String) = Action.async {
+    widgetRepo.remove(BSONDocument(Id -> BSONObjectID(id))).map(result => Accepted)
+  }
+
+}
---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := """play-mongodb-tutorial"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.11.14"
+)
+
+
+
+fork in run := true
